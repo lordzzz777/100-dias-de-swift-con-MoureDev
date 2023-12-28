444ee2fdda53a267bcc65c7a2b6eefefacb04e9a
Esteban <lordzzz@iclud.com>
2023-12-28 22:58:47 +0100

    actulización de clases 28/12/2023

--- MyPlayground.playground/Pages/leccion_1.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/leccion_1.xcplaygroundpage/Contents.swift
@@ -4,4 +4,8 @@ import Foundation
 
 var greeting = "Hello, playground"
 
-//: [Next](@next)
+func sumar(number1: Int, number2: Int ) -> Int {
+    let result = number1 + number2
+    return result
+}
+
